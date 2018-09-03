8e98708bff5cc3fd59eedadb1d59a2f1b2617c74
Randi Prince <randiprince3@macbook-pro-7.attlocal.net>
2018-09-03 11:14:25 -0700

    heroku

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var path = require("path");
 
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(express.static(".app/public"));
+
 
 
 require("./app/routing/apiRoutes.js")(app);
